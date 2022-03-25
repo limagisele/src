55abe3d6ede2410b0498c62219e63645e4504294
limagisele <limagisele05@gmail.com>
2022-03-25 19:23:19 +1100

    create Employee class

--- payroll.rb
+++ payroll.rb
@@ -1,59 +1,90 @@
+require 'date'
+require 'json'
+
+class InvalidUserError < StandardError
+    def message
+        return "Wrong ID and/or password!"
+    end
+end
+
+class Timesheet
+    def initialize(date, start, finish, leave, time)
+        @timesheet = { date: date, start_time: start, finish_time: finish, leave_type: leave, leave_time: time }
+    end
+
+    def get_date
+    end
+
+end
+
 class Employee
     attr_reader :name, :id, :password
+    attr_accessor :timesheets
+
+    @@list_of_employees = []
 
     def initialize(name, id, password)
         @name = name
         @id = id
         @password = password
-    end
-end
-
-class Worker < Employee
-    attr_accessor :timesheets
-
-    @@workers = []
-
-    def initialize(name, id, password)
-        super(name, id, password)
         @timesheets = []
     end
-end
 
-class Manager < Employee
-    def initialize(name, id, password)
-        super(name, id, password)
+    def self.list_of_employees
+        return @@list_of_employees
     end
 
-    def report
-    end
-end
+    def self.find_employee(id, password)
+        found_employee = @@list_of_employees.find { |person| person.id == id && person.password == password }
+        raise(InvalidUserError) if found_employee.nil?
 
-class Timesheet
-    include PayableLeave
-
-    def initialize(date)
-        @date = date
-        @start_time
-        @finish_time
-        @worked_hours = finish - start
-        @leave_taken = {}
+        return found_employee
     end
-    def self.add_time
-    end
-
-    def self.edit_time
-    end
-
 end
 
 module PayableLeave
     def paycode
         @leave = ["sick", "annual", "bereavement", "unpaid", "parental", "long service", "public holiday"]
     end
-    def add_leave
-    end
-    def edit_leave
-    end
 end
 
+
+# date = Date.parse('2022.02.23')
+# puts date
+# puts date.cweek
+
+employees = JSON.load_file('employees.json', symbolize_names: true)
+employees.each { |person| Employee.list_of_employees << Employee.new(person[:name], person[:id], person[:password]) }
+
+puts "Welcome to the Alternative Payroll Program"
+begin
+    print "Please enter your employee ID: "
+    username = gets.chomp.to_i
+    print "Please enter your password: "
+    password = gets.chomp
+    user = Employee.find_employee(username, password)
+rescue InvalidUserError => e
+    system "clear"
+    puts e.message
+    retry
+end
+system "clear"
+puts "Hello, #{user.name}!"
+
+continue = true
+while continue
+    puts "What would you like to do with your timesheet today?"
+    puts "1. Add new entry, 2. Edit existing timesheet, 3. Exit"
+    print "Type 1, 2 or 3: "
+    option = gets.chomp
+    
+    case option
+    when "1"
+
+    when "2"
+
+    when "3"
+        continue = false
+    end
+end
 
--- spec/payroll_spec.rb
+++ spec/payroll_spec.rb
@@ -1,10 +1,26 @@
 require './payroll'
 
 describe Employee do
-    let(:employee) { Employee.new("Mary Smith", 190, 1234) }
-    
-    it 'can be initialized' do
-        expect(employee).to be_instance_of Employee
+    describe '#self.employees' do
+        it 'returns array of employees' do
+            expect(Employee.list_of_employees.length).to eq 3
+        end
     end
 
+    describe '#self.find_employee' do
+        it 'returns an instance of Employee when exists' do
+            found_employee = Employee.find_employee(345, 'abcd')
+            expect(found_employee).to be_an_instance_of Employee
+        end
+        it 'returns nil if employee does not exist' do
+            found_employee = Employee.find_employee('john', 'aaa')
+            expect(found_employee).to be_nil
+        end
+    end
+
+    # describe '#add_timesheet' do
+    #     it 'adds new timesheet into timesheets array of a given employee' do
+    #         employee = Employee.new('sally', 222, 'xyz')
+    #         expect(employee.add_timesheet).to 
 end
+
