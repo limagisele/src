13eed155ae1e9ddfa9cd8c47de35ae8ae8e484dd
limagisele <limagisele05@gmail.com>
2022-04-03 18:24:29 +1000

    add test for #upload_employees

--- spec/payroll_spec.rb
+++ spec/payroll_spec.rb
@@ -7,7 +7,16 @@ describe Timesheet do
     let(:file) { JSON.load_file('json_files/timesheets.json', symbolize_names: true) }
     let(:user_timesheet) { file.find { |employee| employee[:id] == 1111 } }
 
-    # Testing related to the Edit Timesheet feature
+    # Test related to Create and Edit Timesheet features
+    # Times entered by user need to be casted to Integer in order to create an instance of Time in #time
+    # "08" and "09" cannot be casted to Integer so need to delete prefix "0"
+    describe '#time_casting' do
+        it 'eliminates prefix 0 from hour and minutes entered by user' do
+            expect(Timesheet.time_casting(%w[08 00])).to eq(%w[8 0])
+        end
+    end
+
+    # Tests related to the Edit Timesheet feature
     # User can only edit a timesheet if there is one saved in the file matching the date entered by the user
     describe '#timesheet_index' do
         it 'returns array index for the employee\'s timesheet in timesheets.json matching the given date ' do
@@ -21,18 +30,21 @@ describe Timesheet do
             expect(index).to be_nil
         end
     end
-    # Testing related to Add and Edit Timesheet features
-    describe '#time_casting' do
-        it 'eliminates prefix 0 from hour and minutes entered by user' do
-            expect(Timesheet.time_casting(%w[08 00])).to eq(%w[8 0])
-        end
-    end
 end
 
-# This test protects the Sign-in feature
-# User needs to match employee ID and password
-# User needs to be an instance of Employee class
 describe Employee do
+    # Test to confirm employees.json has been made loaded into @list_of_employees
+    # as instances of Employee class
+    describe '#upload_employees' do
+        it 'creates instances of Employee' do
+            Employee.upload_employees
+            expect(Employee.list_of_employees[0]).to be_an_instance_of Employee
+        end
+    end
+
+    # Tests to protect the Sign-in feature
+    # User needs to match employee ID and password
+    # User needs to be an existing instance of Employee class
     describe '#find_employee' do
         it 'returns an instance of Employee when exists' do
             employee = Employee.find_employee(3333, 'xyz456')
